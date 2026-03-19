f8208493bf3e37603c3b3e23da21c0fce86825d9
harshini15092006 <harshini.jakka15@gmail.com>
2026-03-19 20:36:15 +0530

    Fix CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://suraksha-pay.netlify.app',
-    'https://famous-malasada-ef37e2.netlify.app'
-  ],
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 app.use(express.json());
 
